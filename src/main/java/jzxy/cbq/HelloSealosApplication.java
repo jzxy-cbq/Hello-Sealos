469327a2640c51b119a26f72fcc7edcfce0e1de0
cbq <2024cbq@gmail.com>
2024-06-30 16:10:18 +0800

    fix: change db to postgreSQL

--- src/main/java/jzxy/cbq/HelloSealosApplication.java
+++ src/main/java/jzxy/cbq/HelloSealosApplication.java
@@ -1,13 +1,17 @@
 package jzxy.cbq;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class HelloSealosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSealosApplication.class, args);
+
+		log.info("HelloSealosApplication run successful ");
 	}
 
 }
---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO tb_person (id, address, age, email, name) VALUES (1, '山西省晋中市榆次区', 22, '2024cbq@gmail.com', 'c''b''q');
